7fcde3985d726411b584fe824a0f80b03b814d83
endes <en.desimone@outlook.it>
2023-11-11 12:35:42 +0100

    fix

--- bin/firebase_verify_token_dart.dart
+++ bin/firebase_verify_token_dart.dart
@@ -1,4 +1,13 @@
-import 'package:firebase_verify_token_dart/firebase_verify_token_dart.dart'
-    as firebase_verify_token_dart;
+import 'package:firebase_verify_token_dart/firebase_verify_token_dart.dart';
 
-void main(List<String> arguments) {}
+void main() async {
+  FirebaseVerifyToken.projectId = '';
+
+  final result = await FirebaseVerifyToken.verify('token');
+
+  if (result) {
+    print('TOKEN IS VERIFIED');
+  } else {
+    print('TOKEN IS NOT VERIFIED');
+  }
+}
